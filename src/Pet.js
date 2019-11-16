63f5eccfca86bb14621c6885ba5c8fd39d7e09d1
harvest <wottinger@trhc.com>
2019-11-16 12:10:12 -0500

    04 - App JSX, Pet edits

--- src/Pet.js
+++ src/Pet.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Pet = ({ name, animal, breed }) => {
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>Name: {name}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
